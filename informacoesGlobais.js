d0dbab1284b1662c5e5b637c5cfeb009617d71ad
ManuXD <126265298+ManuSam001@users.noreply.github.com>
2024-10-29 07:41:03 -0300

    .

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,15 +1,21 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/romulopena/ciencia-de-dados/refs/heads/main/basededados/educacao-dados-globais.json&#39;
 
-async function vizualizarInformacoesGlobais() {
-  const res = await fetch(url)
-  const dados = await res.json()
-
-  const paragrafo = document.createElement('p')
-  paragrafo.classList.add('graficos-container__texto')
-  paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
-  
-  const container = document.getElementById(‘graficos-container’)
-  container.appendChild(paragrafo)
+async function visualizarInformacoesGlobais(){
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasComAcesso = (dados.total_pessoas_com_acesso_a_educacao / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio_dia_estudando)
+    //const minutos = Math.round((dados.tempo_medio_dia_estudando - horas) * 100)
+    const pessoasEstudando = ((pessoasComAcesso / pessoasNoMundo) * 100).toFixed(2)
+    //console.log(dados)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> bilhões de pessoas e que aproximadamente <span>${pessoasEstudando}%</span> delas estão fazendo algum curso e passam em média <span>${horas} horas</span> estudando?`
+   
+   
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
+visualizarInformacoesGlobais();
